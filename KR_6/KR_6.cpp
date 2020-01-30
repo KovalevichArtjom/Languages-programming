3ec624e9ee3d2fd59e7afa8f8893cb956e8b5bc6
Artjom <kowalevichartem@mail.ru>
2020-01-31 01:34:07 +0300

    Completed working on KR_6(Recursion).

---
+++ KR_6/KR_6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+auto NOD(int N, int M)
+{
+    if (M == 0 || N == 0) {
+        return M == 0 ? M : N;
+    } else if (fmod(M, N) == 0) {
+        return N;
+    } else {
+        return NOD(fmod(M, N), N);
+    }
+}
+
+int main()
+{
+    int N, M;
+
+    cout << "Enter N with type[int]: ";
+    cin >> N;
+    cout << endl << "Enter M with type[int]: ";
+    cin >> M;
+
+    cout << endl << "Result completed function NOD: " << NOD(N, M) << endl;
+
+	cin.get();
+	return 0;
+}
