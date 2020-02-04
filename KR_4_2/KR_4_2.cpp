037c920e765aa06940d7442fdd1d878d8d20ca40
Artjom <kowalevichartem@mail.ru>
2020-02-04 21:20:32 +0300

    Completed on Minimum working KR_4_2(Created Abstract Base Class).

---
+++ KR_4_2/KR_4_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class figure {
+public:
+	int colorFigure;
+
+	virtual void create() = 0;
+	virtual void remove() = 0;
+	virtual void draw() = 0;
+	virtual void setSize() =0;
+	virtual void setCoordinate() = 0;
+	virtual void setColorFigure(int color) {
+		this->colorFigure = color;
+	}
+};
+
+
+int main()
+{
+
+	cin.get();
+
+	return 0;
+}
